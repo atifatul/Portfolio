471990fd924babc2fada7e2567e06e4ffd66ae0a
Atif <atifatul752@gmail.com>
2025-08-02 11:15:19 +0530

    add project section

--- app.js
+++ app.js
@@ -5,4 +5,21 @@ document.querySelectorAll(".card").forEach(card => {
         card.style.setProperty("--x", `${e.clientX - rect.left}px`);
         card.style.setProperty("--y", `${e.clientY - rect.top}px`);
     });
-});
+}); 
+
+const video1= document.getElementById('projectVideo1');
+const video2= document.getElementById('projectVideo2');
+const video3= document.getElementById('projectVideo3');
+const video4= document.getElementById('projectVideo4');
+ 
+const videoList =[video1, video2, video3, video4];
+
+videoList.forEach(function(video){
+    video.addEventListener("mouseover",function(){
+        video.play()
+    })
+    video.addEventListener("mouseout",function(){
+        video.pause()
+    })
+
+})
